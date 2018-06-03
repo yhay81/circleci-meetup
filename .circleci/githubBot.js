04d853f0e496a19430b65bf93639343efb23b1d2
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-04 06:14:17 +0900

    change to comment

--- .circleci/githubBot.js
+++ .circleci/githubBot.js
@@ -25,9 +25,10 @@ const USERNAME = process.env["CIRCLE_PROJECT_USERNAME"];
 const REPO = process.env["CIRCLE_PROJECT_REPONAME"];
 const PR = process.env["CI_PULL_REQUEST"];
 const ISSUE_NUMBER = PR ? basename(process.env["CI_PULL_REQUEST"]) : "";
-// const AUTH = "30006bc2ad860812ac21c290a1c674df3506ac31";
+// const AUTH = "cfe7554b1bdf8b97814cde1fe3a4f2c580bf627c";
 // const USERNAME = "yhay81";
 // const REPO = "circleci-meetup";
+// const ISSUE_NUMBER = "12";
 console.log(AUTH);
 const bot = new githubBot(AUTH, USERNAME, REPO, ISSUE_NUMBER);
 
