0e126f40babf170516dddf21241728f86671c595
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-04 05:50:05 +0900

    change to comment

--- .circleci/comment.js
+++ .circleci/comment.js
@@ -1,6 +1,5 @@
 const bot = require("./githubBot");
-const { basename } = require("path");
-const issueNumber = basename("CI_PULL_REQUEST");
+const issueNumber = process.env["CI_PULL_REQUEST"];
 let body = `
 <h3>From CircleCI</h3>
 Demo: <strong>Hello world</strong>
