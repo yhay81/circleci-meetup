16ac0fa0943bf827a392551212d45ed4f953b85b
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-04 06:34:32 +0900

    change to comment

--- .circleci/githubBot.js
+++ .circleci/githubBot.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { basename } = require("path");
 class githubBot {
   constructor(auth, username, repo, issueNumber) {
     this.url = `https://${auth}:x-oauth-basic@api.github.com/repos/${username}/${repo}`;
@@ -23,6 +24,8 @@ const AUTH = process.env["GH_AUTH_TOKEN"];
 const USERNAME = process.env["CIRCLE_PROJECT_USERNAME"];
 const REPO = process.env["CIRCLE_PROJECT_REPONAME"];
 const ISSUE_NUMBER = process.env["CIRCLE_PR_NUMBER"];
+const PR = process.env["CI_PULL_REQUEST"];
+const ISSUE_NUMBER = PR ? basename(process.env["CI_PULL_REQUEST"]) : "";
 // const AUTH = "";
 // const USERNAME = "yhay81";
 // const REPO = "circleci-meetup";
