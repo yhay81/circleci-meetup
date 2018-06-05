f0bf34e71da25a1ff7d349325aedb35a223bb5e8
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-05 19:52:07 +0900

    Update to initial state

--- .circleci/app.js
+++ .circleci/app.js
@@ -1,28 +1,54 @@
+// You can see these variables in CircleCI builds page
+
+// BASH_ENV=/tmp/.bash_env-5b14d283faad6600155ac668-0-build
+// CI=true
+// CIRCLECI=true
+// CIRCLE_BRANCH=another
+// CIRCLE_BUILD_NUM=59
+// CIRCLE_BUILD_URL=https://circleci.com/gh/yhay81/circleci-meetup/59
+// CIRCLE_COMPARE_URL=
+// CIRCLE_JOB=build
+// CIRCLE_NODE_INDEX=0
+// CIRCLE_NODE_TOTAL=1
+// CIRCLE_PREVIOUS_BUILD_NUM=58
+// CIRCLE_PROJECT_REPONAME=circleci-meetup
+// CIRCLE_PROJECT_USERNAME=yhay81
+// CIRCLE_PULL_REQUEST=https://github.com/yhay81/circleci-meetup/pull/24
+// CIRCLE_PULL_REQUESTS=https://github.com/yhay81/circleci-meetup/pull/24
+// CIRCLE_REPOSITORY_URL=git@github.com:yhay81/circleci-meetup.git
+// CIRCLE_SHA1=c11ed97455793108be25c1a7f7801112e749fcc1
+// CIRCLE_SHELL_ENV=/tmp/.bash_env-5b14d283faad6600155ac668-0-build
+// CIRCLE_STAGE=build
+// CIRCLE_USERNAME=yhay81
+// CIRCLE_WORKING_DIRECTORY=~/repo
+// CI_PULL_REQUEST=https://github.com/yhay81/circleci-meetup/pull/24
+// CI_PULL_REQUESTS=https://github.com/yhay81/circleci-meetup/pull/24
+
 const axios = require("axios");
 const { basename } = require("path");
-const AUTH = process.env["GH_AUTH_TOKEN"];
-const USERNAME = process.env["CIRCLE_PROJECT_USERNAME"];
-const REPO = process.env["CIRCLE_PROJECT_REPONAME"];
-const PR = process.env["CI_PULL_REQUEST"];
-const ISSUE_NUMBER = PR ? basename(PR) : "";
-// const AUTH = "";
-// const USERNAME = "yhay81";
-// const REPO = "circleci-meetup";
-// const ISSUE_NUMBER = "19";
+// const AUTH = process.env["GH_AUTH_TOKEN"];
+// const USERNAME = process.env["CIRCLE_PROJECT_USERNAME"];
+// const REPO = process.env["CIRCLE_PROJECT_REPONAME"];
+// const PR = process.env["CI_PULL_REQUEST"];
+// const ISSUE_NUMBER = PR ? basename(PR) : "";
+const AUTH = "";
+const USERNAME = "yhay81";
+const REPO = "circleci-meetup";
+const ISSUE_NUMBER = "19";
 
 const url = `https://${AUTH}:x-oauth-basic@api.github.com/repos/${USERNAME}/${REPO}/issues/${ISSUE_NUMBER}/comments`;
-// const body = `
-//   <h3>From CircleCI</h3>
-//   Demo: <strong>Hello world</strong>
-//   `;
-
-const { execSync } = require("child_process");
-const testResult = execSync("yarn test")
-  .toString("utf8")
-  .trim();
 const body = `
-<pre>${testResult}</pre>
-`;
+  <h3>From CircleCI</h3>
+  Demo: <strong>Hello world</strong>
+  `;
+
+// const { execSync } = require("child_process");
+// const testResult = execSync("yarn test")
+//   .toString("utf8")
+//   .trim();
+// const body = `
+// <pre>${testResult}</pre>
+// `;
 
 axios
   .post(url, { body })
--- test/test.js
+++ test/test.js
@@ -2,10 +2,27 @@ const { expect } = require("chai");
 const fizzbuzz = require("../src");
 
 describe("fizzbuzz", () => {
-  it("", () => {
+  it("should return number as it is when the argument is not multiples of 3 nor multiples of 5", () => {
     expect(fizzbuzz(1)).to.eql(1);
+    expect(fizzbuzz(2)).to.eql(2);
+    expect(fizzbuzz(4)).to.eql(4);
+  });
+
+  it("should return 'fizz' when the argument is multiples of 3 (and not multiples of 5)", () => {
+    expect(fizzbuzz(3)).to.eql("fizz");
     expect(fizzbuzz(6)).to.eql("fizz");
+    expect(fizzbuzz(9)).to.eql("fizz");
+  });
+
+  it("should return 'buzz' when the argument is multiples of 5 (and not multiples of 3)", () => {
+    expect(fizzbuzz(5)).to.eql("buzz");
     expect(fizzbuzz(10)).to.eql("buzz");
+    expect(fizzbuzz(20)).to.eql("buzz");
+  });
+
+  it("should return 'fizzbuzz' when the argument is multiples of 3 and multiples of 5", () => {
     expect(fizzbuzz(15)).to.eql("fizzbuzz");
+    expect(fizzbuzz(30)).to.eql("fizzbuzz");
+    expect(fizzbuzz(45)).to.eql("fizzbuzz");
   });
 });
