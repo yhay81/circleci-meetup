3934b17855f924caffcbb566f69f0f97e5e591d5
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-05 23:48:43 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const fizzbuzz = function(n) {
+  if (n % 3 === 0 && n % 5 === 0) {
+    return "fizzbuzz";
+  }
+  
   if (n % 3 === 0) {
     return "fizz";
   }
 
   if (n % 5 === 0) {
     return "buzz";
-  }
-
-  if (n % 3 === 0 && n % 5 === 0) {
-    return "fizzbuzz";
   }
 
   return n;
