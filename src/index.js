6e6b9e7321f2f3f7ea82d29180763dddf121e3fe
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-06 20:29:17 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const fizzBuzz = function(n) {
+  // But, we want to return 'Fizz Buzz' if n is multiples of 3 and multiples of 5
+  if (n % 3 === 0 && n % 5 === 0) {
+    return "Fizz Buzz";
+  }
+  
   // return 'Fizz' if n is multiples of 3
   if (n % 3 === 0) {
     return "Fizz";
@@ -9,10 +14,7 @@ const fizzBuzz = function(n) {
     return "Buzz";
   }
 
-  // But, we want to return 'Fizz Buzz' if n is multiples of 3 and multiples of 5
-  if (n % 3 === 0 && n % 5 === 0) {
-    return "Fizz Buzz";
-  }
+
 
   return n;
 };
